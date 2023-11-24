5a8b6e559c79e4679993e5627f82c2d19f152e6b
Iskander Bikmullin <iskanderbikmullin36@gmail.com>
2023-11-24 18:45:53 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Page4.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page4.xaml
+    /// </summary>
+    public partial class Page4 : Page
+    {
+        public Page4()
+        {
+            InitializeComponent();
+        }
+        private void Math_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(BB.Text);
+            int k = int.Parse(AA.Text);
+            double? y = 0;
+
+            switch (k)
+            {
+                case 3: Rez.Text = $"y = {Sin(x) + 2}"; break;
+                case 20: Rez.Text = $"y = {Cos(Pow(x, 2))}"; break;
+                case int t when t == 10 || t == 15: Rez.Text = $"y = { Tan(x) + Sin(x)}"; break;
+                default: Rez.Text = $"y = Неопределено"; break;
+            }
+        }
+    }
+}
---
+++ WpfApp2/Page5.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page5.xaml
+    /// </summary>
+    public partial class Page5 : Page
+    {
+        public Page5()
+        {
+            InitializeComponent();
+        }
+        private void Math_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(AA.Text);
+            double y = double.Parse(BB.Text);
+            string s = "";
+
+            if (Pow(x - 0, 2) + Pow(y - 0, 2) <= Pow(10, 2) && y <= Abs(x))
+            {
+                if (Pow(x - 0, 2) + Pow(y - 0, 2) == Pow(10, 2))
+                {
+                    s = "На границе";
+                }
+                else { s = "Да"; }
+            }
+            else { s = "Нет"; }
+            Rez.Text = $"{s}";
+        }
+    }
+}
